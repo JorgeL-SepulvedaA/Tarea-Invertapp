65f9f5f68a91911b17a72e89f3b74f4fab2e160b
Jorge Sepulveda <202010714@itla.edu.do>
2021-07-05 03:27:53 -0400

    Classes and Interfaces Creation

---
+++ IMetodosMantenimiento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertApp
+{
+    interface IMetodosMantenimiento
+    {
+        void Agregar();
+        void Editar();
+        void Elminar();
+        void Listar();
+        void Volver();
+    }
+}
