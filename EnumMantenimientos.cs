10b7a865755010b24838d8c0618d193bb9dba8f2
Jorge Sepulveda <202010714@itla.edu.do>
2021-07-05 02:49:40 -0400

    Enum Creation

---
+++ EnumMantenimientos.cs
@@ -0,0 +1,8 @@
+﻿enum EnumMantenimientos
+{
+    Agregar = 1,
+    Editar = 2,
+    Eliminar = 3,
+    Listar = 4,
+    Volver = 5
+}
---
+++ EnumMenuPrincipal.cs
@@ -0,0 +1,8 @@
+﻿enum EnumMenuPrincipal
+{
+    MantenimientosCategorias = 1,
+    MantenimientosProductos = 2,
+    EntradaInventario = 3,
+    SalidaInventario = 4,
+    Salir = 5
+}
